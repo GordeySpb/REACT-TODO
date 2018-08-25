b2d3c2dd6ebc94e13cebb8117f9f654b68cc12cd
Kirill Gordeew <gordey_89@mail.ru>
2018-08-25 17:48:29 +0300

    добавил редьюсер для todo

---
+++ src/reducers/todos.js
@@ -0,0 +1,25 @@
+import {
+  ADD_TODOS, ADD_TODO, DELETE, TOGGLE,
+} from '../helpers/actions';
+
+const initialState = [];
+
+export default function todos(state = initialState, { type, payload }) {
+  switch (type) {
+    case ADD_TODOS:
+      return payload;
+    case ADD_TODO:
+      return [...state, payload];
+    case DELETE:
+      return state.filter(elem => elem.id !== +payload.id);
+    case TOGGLE:
+      return state.map((item) => {
+        if (item.id === payload.id) {
+          return { ...payload };
+        }
+        return item;
+      });
+    default:
+      return state;
+  }
+}
