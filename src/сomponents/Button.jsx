1177b46ebac8ada2a7ec32a91c8cca4a46556f42
GordeySpb <gordey_89@mail.ru>
2018-09-09 19:44:08 +0300

    правит стили для Button

--- src/сomponents/Button.jsx
+++ src/сomponents/Button.jsx
@@ -14,7 +14,7 @@ const StyledButton = styled.button`
   color: white;
   box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);
     &:hover {
-      cursor pointer;
+      cursor: pointer;
       background-color: #35cef4;
     }
 `;
